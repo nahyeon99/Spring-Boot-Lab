07e2132987ae011362a311a71e5b47cdd4e1f28e
nahyeon99 <nahyeonee99@gmail.com>
2024-06-21 18:12:03 +0900

    style: 메서드 네이밍 변경

--- effective-java/src/main/java/com/study/effective_java/item01/Product.java
+++ effective-java/src/main/java/com/study/effective_java/item01/Product.java
@@ -2,8 +2,8 @@ package com.study.effective_java.item01;
 
 public class Product {
 	public static void main(String[] args) {
-		Settings settings1 = Settings.newInstance();
-		Settings settings2 = Settings.newInstance();
+		Settings settings1 = Settings.getInstance();
+		Settings settings2 = Settings.getInstance();
 
 		System.out.println("settings1 = " + settings1);
 		System.out.println("settings2 = " + settings2);
--- effective-java/src/main/java/com/study/effective_java/item01/Settings.java
+++ effective-java/src/main/java/com/study/effective_java/item01/Settings.java
@@ -1,5 +1,9 @@
 package com.study.effective_java.item01;
 
+/**
+ * 이 클래스의 인스턴스는 $getInstance()를 통해 사용한다.
+ * @see #getInstance()
+ */
 public class Settings {
 
 	private boolean useAutoSteering;
@@ -13,7 +17,7 @@ public class Settings {
 
 	private static final Settings SETTINGS = new Settings();
 
-	public static Settings newInstance() {
+	public static Settings getInstance() {
 		return SETTINGS;
 	}
 
